261273ea8c23f5deeaf7aa92f7d777d042385bbc
kcmjschwartz <kcmjschwartz@gmail.com>
2021-06-01 15:28:15 -0400

    login and signup fields created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, {useState} from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Auth from './components/Auth/Auth';
 
 function App() {
 
-  const [sessionToken, setSessionToken] = useState("Token");
+  const [sessionToken, setSessionToken] = useState();
   console.log(sessionToken);
 
   return (
     <div className="App">
       <Navbar setSession={sessionToken}/>
-      <p> This is a test!</p>
       {sessionToken}
+      <Auth />
     </div>
   );
 }
---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,72 @@
+import React, {useState}from 'react';
+import './Auth.css';
+
+const Auth = (props) => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName]  = useState('');
+    const [email, setEmail]  = useState('');
+    const [password, setPassword]  = useState('');
+    const [login, setLogin] = useState(true);
+
+    const title = () => {
+        return login ? 'Login' : 'Signup';
+        //if login is true, return Login.  If login is false, return Signup
+    }
+
+    const loginToggle = (event) => {
+        //Takes in an event so we can stop the page from reloading on the form submission
+        event.preventDefault();
+        
+        setLogin(!login);
+        //set login to the opposite of it's current value
+
+        setEmail('');
+        setPassword('');
+        setFirstName('');
+        setLastName('');
+    }
+
+    const signupFields = () => !login ? 
+    (
+        <div>
+            <label htmlFor= 'firstName'>First Name</label>
+            <br/>
+            <input type='text' id='firstName' value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+            <br/>
+            <label htmlFor= 'lastName'>Last Name</label>
+            <br/>
+            <input type='text' id='lastName' value={lastName} onChange={(e) => setLastName(e.target.value)} />
+            </div>
+    ) :null;
+
+ // If login is false, we want to show the additional fields.  If login is true, show nothing(null)   
+
+    return(
+        <div>
+            <form>
+                <h1>{title()}</h1>
+                <br/>
+                {signupFields()}
+                <label htmlFor='email'>Email:</label>
+                <br/>                
+                <input type = 'text' id='email' value= {email} onChange={(e) => {setEmail(console.log(e), e.target.value)}} />
+                <br/>
+                <label htmlFor='password'>Password</label>
+                <br/>
+                <input type='password' id='password' value={password} onChange={(e) => {setPassword(e.target.value)}} />
+                <br/>
+                <button onClick={loginToggle}>Login/Signup Toggle</button>
+                <br/>            
+                <button type='submit'>Submit User Data</button>
+            </form>
+        </div>
+    )
+}
+
+/*
+- if we take out OnChange handler, the value is locked because the value is an empty string
+- the onChange handler takes in an anonymous function that will set the state of our variables top what we type in the input
+
+Two-way binding: This is essentially a circuit. The data comes in and changes the state variable via setEmail/setPassword, and the state variable is tied to the input field via the value attribute.
+*/
+export default Auth;
